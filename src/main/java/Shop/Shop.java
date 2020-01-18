f868edc65943fd6832e088ddd847d4f4e33d2cc6
naz_liton <nazrulliton@icloud.com>
2020-01-18 14:12:06 +0000

    shop class created

--- src/main/java/Shop/Shop.java
+++ src/main/java/Shop/Shop.java
@@ -1,4 +1,15 @@
 package Shop;
 
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+    private ArrayList<ISell> stock;
+
+    public void Shop(){
+        this.stock = new ArrayList<ISell>();
+    }
+
+ 
 }
