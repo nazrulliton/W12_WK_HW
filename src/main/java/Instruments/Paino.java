ca7887ca96da1538cb0e35fb77d097ac0116d54c
naz_liton <nazrulliton@icloud.com>
2020-01-18 11:45:16 +0000

    piano tests now all passing

--- src/main/java/Instruments/Paino.java
+++ src/main/java/Instruments/Paino.java
@@ -1,5 +1,13 @@
 package Instruments;
 
-public class Paino {
+public class Paino extends Instrument{
+    private double numOfKeys;
+    public Paino(String colour, String type, double buyPrice, double sellPrice, String brand, double numOfKeys) {
+        super(colour, type, buyPrice, sellPrice, brand);
+        this.numOfKeys = numOfKeys;
+    }
 
+    public double getNumOfKeys() {
+        return numOfKeys;
+    }
 }
---
+++ src/test/java/PianoTest.java
@@ -0,0 +1,17 @@
+import Instruments.Paino;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PianoTest {
+    Paino paino;
+    @Before
+    public void before(){
+        paino = new Paino("Black", "paino", 20.00, 40.00, "Helms", 40 );
+    }
+    @Test
+    public void canGetNumOfKeys(){
+        assertEquals(40,paino.getNumOfKeys(),0.01 );
+    }
+}
