6be2f1e63d035936aa0130ea2c1c56b532ac4b53
naz_liton <nazrulliton@icloud.com>
2020-01-18 14:21:51 +0000

    name and till property added to shop

--- src/main/java/Shop/Shop.java
+++ src/main/java/Shop/Shop.java
@@ -1,17 +1,25 @@
 package Shop;
 
+import Instruments.Sax;
 import behaviours.ISell;
 
 import java.util.ArrayList;
 
 public class Shop {
+    private String name;
     private ArrayList<ISell> stock;
+    private double till;
+
+    public void Shop(String name, double till){
+        this.name = name;
+        this.till = till;
+        stock = new ArrayList<ISell>();
+
+    }
 
     public int getStockSize(){
         return stock.size();
     }
-
-
 
     public void addToStock(ISell item){
         stock.add(item);
@@ -22,6 +30,11 @@ public class Shop {
     }
 
 
+    public String getName() {
+        return name;
+    }
 
-
+    public double getTill() {
+        return till;
+    }
 }
