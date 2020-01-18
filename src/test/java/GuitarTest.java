7cc6ce6fad0c8a60abd0b0a2d266dead211b1e15
naz_liton <nazrulliton@icloud.com>
2020-01-18 11:37:28 +0000

    all test passing for G class

--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -12,8 +12,28 @@ public class GuitarTest {
         guitar = new Guitar("red", "Guitar", 10.00, 20.00, "Nike", 10.00);
     }
     @Test
+    public void hasType(){
+        assertEquals("Guitar", guitar.getType());
+    }
+    @Test
     public void hasColour(){
-        assertEquals("red", guitar.getColour() );
+        assertEquals("red", guitar.getColour());
+    }
+    @Test
+    public void hasBuyPrice(){
+        assertEquals(10.00, guitar.getBuyPrice(),0.01);
+    }
+    @Test
+    public void hasSellPrice(){
+        assertEquals(20.00, guitar.getSellPrice(),0.01);
+    }
+    @Test
+    public void hasBrand(){
+        assertEquals("Nike", guitar.getBrand());
+    }
+    @Test
+    public void hasNumOfStrings(){
+        assertEquals(10.00, guitar.getNumOfStrings(),0.01);
     }
 
 }
