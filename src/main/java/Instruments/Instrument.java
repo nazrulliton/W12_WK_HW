0dc01cfbe6af225fb862d9bb0fd9dcbab1afe2ab
naz_liton <nazrulliton@icloud.com>
2020-01-18 11:26:25 +0000

    Instrument class created along w/getters/setters

--- src/main/java/Instruments/Instrument.java
+++ src/main/java/Instruments/Instrument.java
@@ -1,7 +1,51 @@
 package Instruments;
 
 public abstract class Instrument {
-    
+    private  String colour;
+    private  String type;
+    private  double buyPrice;
+    private  double sellPrice;
+    private String brand;
 
+    public Instrument(String colour, String type, double buyPrice, double sellPrice, String brand){
+        this.colour = colour;
+        this.type = type;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        this.brand = brand;
+    }
+
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
 }
 
