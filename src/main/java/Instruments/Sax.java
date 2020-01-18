12f23f1b7508e321dd3ccbf085b6f2c4df277d2f
naz_liton <nazrulliton@icloud.com>
2020-01-18 11:48:57 +0000

    all tests passing for sax class

--- src/main/java/Instruments/Sax.java
+++ src/main/java/Instruments/Sax.java
@@ -1,5 +1,13 @@
 package Instruments;
 
-public class Sax {
+public class Sax extends Instrument{
+    private double numOfValves;
+    public Sax(String colour, String type, double buyPrice, double sellPrice, String brand, double numOfValves) {
+        super(colour, type, buyPrice, sellPrice, brand);
+        this.numOfValves = numOfValves;
+    }
 
+    public double getNumOfValves() {
+        return numOfValves;
+    }
 }
---
+++ src/test/java/SaxTest.java
@@ -0,0 +1,18 @@
+import Instruments.Sax;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SaxTest {
+    Sax sax;
+    @Before
+    public void before(){
+        sax = new Sax ("Red", "sax", 15.00, 45.00, "Dopsi", 30);
+    }
+    @Test
+    public void canGetNumOfValves(){
+        assertEquals(30, sax.getNumOfValves(), 0.01 );
+    }
+
+}
